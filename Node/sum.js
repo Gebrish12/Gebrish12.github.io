9c55bf1491c65ed31ed105f5fe7ee7cb585491b2
Gebrish12 <87670636+Gebrish12@users.noreply.github.com>
2021-08-06 00:58:06 -0500

    Create sum.js

---
+++ Node/sum.js
@@ -0,0 +1,20 @@
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let sum = 0;
+
+function getNumber() {
+    readline.question('Enter number or type stop: ', input => {
+        if (input.trim() === "stop") {
+            console.log(`Sum of all the numbers is: ${sum}`)
+            readline.close();
+        } else {
+            sum += parseInt(input);
+            getNumber();
+        }
+    });
+}
+
+getNumber();
